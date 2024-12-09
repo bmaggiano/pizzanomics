787384d0b075ddd1d306d871f36e30625138aef6
bmaggiano <brandon.maggiano@gmail.com>
2024-12-09 13:35:12 -0700

    added helpers util to get users pizza and toppings

---
+++ app/utils/helpers.ts
@@ -0,0 +1,15 @@
+import prisma from "../clients/prismaClient";
+export const getToppings = async () => {
+  const toppings = await prisma.topping.findMany();
+  return toppings;
+};
+
+export const getPizzas = async () => {
+  const pizzas = await prisma.pizza.findMany();
+  return pizzas;
+};
+
+export const getUser = async () => {
+  const users = await prisma.user.findMany();
+  return users;
+};
