86807b89bc20bd37ea925c10f3f5f46eee3028f2
bmaggiano <brandon.maggiano@gmail.com>
2024-12-10 09:38:43 -0700

    modified desktop nav to have mobile capablities as well

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,31 +1,52 @@
 import { Button } from "@/components/ui/button";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu, Pizza, HopOff } from "lucide-react";
+import MobileNavigation from "./mobileNav";
 
 const navItems = [
-  { name: "Pizzas", href: "#pizzas" },
-  { name: "Toppings", href: "#toppings" },
+  { name: "Pizzas", href: "#pizzas", icon: <Pizza /> },
+  { name: "Toppings", href: "#toppings", icon: <HopOff /> },
 ];
 
 export default function Navbar() {
   return (
     <div className="bg-white">
       <nav className="flex justify-between items-center p-4">
-        <div className="flex items-center">
+        <div className="flex items-center sm:justify-between w-full">
           <img src="/pizza.png" alt="Pizzanomics Logo" className="h-8 mr-4" />
           <h1 className="text-2xl tracking-tight font-bold">Pizzanomics</h1>
-          <ul className="ml-4 flex items-center gap-4">
-            {navItems.map((item) => (
-              <li key={item.name}>
-                <a
-                  href={item.href}
-                  className="text-gray-500 hover:text-gray-700"
-                >
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
+          {/* for desktop */}
+          <div className="hidden sm:flex sm:justify-between sm:w-full">
+            <ul className="ml-4 flex items-center gap-4">
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className="text-gray-600 font-medium hover:text-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <Button className="font-semibold" variant={"outline"}>
+              Login
+            </Button>
+          </div>
         </div>
-        <Button variant={"outline"}>Login</Button>
+        {/* for mobile */}
+        <div className="sm:hidden">
+          <MobileNavigation />
+        </div>
       </nav>
     </div>
   );
