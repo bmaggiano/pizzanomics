2e35089a51b8f3f0c9fcdd02b41f325db72b3d88
bmaggiano <brandon.maggiano@gmail.com>
2024-12-10 12:12:27 -0700

    rm navbar from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use server";
 import Hero from "./hero";
-import Navbar from "./navbar";
 import Pizzas from "./pizzas";
 import Toppings from "./toppings";
 import { getToppings, getPizzas } from "./utils/helpers";
@@ -10,7 +9,6 @@ export default async function Home() {
   const pizzas = await getPizzas();
   return (
     <>
-      <Navbar />
       <Hero />
       <Pizzas pizzas={pizzas} />
       <Toppings toppings={toppings} />
