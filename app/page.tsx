eea8b0c91b581b352e150c1b76d586a2f139ddbb
bmaggiano <brandon.maggiano@gmail.com>
2024-12-09 17:24:20 -0700

    early stage initial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+"use server";
+import Hero from "./hero";
+import Navbar from "./navbar";
+import Pizzas from "./pizzas";
+import Toppings from "./toppings";
+import { getToppings, getPizzas } from "./utils/helpers";
+
+export default async function Home() {
+  const toppings = await getToppings();
+  const pizzas = await getPizzas();
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Pizzas pizzas={pizzas} />
+      <Toppings toppings={toppings} />
+      <div className="mt-60" id="toppings">
+        toppings
+      </div>
+    </>
+  );
 }
