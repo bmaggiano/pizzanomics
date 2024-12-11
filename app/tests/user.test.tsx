c75b8d0a53e9ac4b20eff2abbcedac49ecdef680
bmaggiano <brandon.maggiano@gmail.com>
2024-12-11 11:52:33 -0700

    changed verbiage on test for get users

--- app/tests/user.test.tsx
+++ app/tests/user.test.tsx
@@ -1,8 +1,8 @@
-import { getUser } from "../utils/helpers";
+import { getUsers } from "../utils/helpers";
 
 describe("User Model", () => {
   it("should retrieve a list of users", async () => {
-    const users = await getUser(); // Use await to get the resolved data
+    const users = await getUsers(); // Use await to get the resolved data
 
     // Ensure that users is an array
     expect(users).toBeInstanceOf(Array);
