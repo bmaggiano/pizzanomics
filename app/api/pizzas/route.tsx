49f3f76d5f3d55ae9cdd409c397e54005248e3cd
bmaggiano <brandon.maggiano@gmail.com>
2024-12-11 11:54:25 -0700

    include toppings with pizzas

--- app/api/pizzas/route.tsx
+++ app/api/pizzas/route.tsx
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import prisma from "../../clients/prismaClient";
 export async function GET() {
-  const pizzas = await prisma.pizza.findMany();
+  const pizzas = await prisma.pizza.findMany({
+    include: {
+      toppings: true,
+    },
+  });
   return NextResponse.json({ pizzas });
 }
