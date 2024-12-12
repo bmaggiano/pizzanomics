ca2d7c8f6b41c75fa2b4dedae90dc9869cae38f6
bmaggiano <brandon.maggiano@gmail.com>
2024-12-12 12:56:51 -0700

    slight tweak on mobile opacity

--- app/hero.tsx
+++ app/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero({ user }: { user?: User }) {
       className="relative bg-cover bg-center py-12 sm:py-32"
       style={{ backgroundImage: "url('/pizzalogo2.avif')" }}
     >
-      <div className="absolute inset-0 bg-black opacity-45"></div>
+      <div className="absolute inset-0 bg-black opacity-70 sm:opacity-45"></div>
       <div className="container mx-auto px-4 relative z-10">
         <div className="max-w-lg text-white">
           <h2 className="text-5xl font-bold mb-4">Discover the Art of Pizza</h2>
