11247f3a436a5ba58bcb1e3b2ea350c85fab7ea1
bmaggiano <brandon.maggiano@gmail.com>
2024-12-11 21:29:58 -0700

    hero section opacity

--- app/hero.tsx
+++ app/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       className="relative bg-cover bg-center py-12 sm:py-32"
       style={{ backgroundImage: "url('/pizzalogo2.avif')" }}
     >
-      <div className="absolute inset-0 bg-black opacity-75"></div>
+      <div className="absolute inset-0 bg-black opacity-45"></div>
       <div className="container mx-auto px-4 relative z-10">
         <div className="max-w-lg text-white">
           <h2 className="text-5xl font-bold mb-4">Discover the Art of Pizza</h2>
